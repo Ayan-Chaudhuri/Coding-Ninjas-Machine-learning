4d269898dc88f927404643d20ffd87f0b12e2389
Ayan Chaudhuri <142921592+Ayan-Chaudhuri@users.noreply.github.com>
2023-12-22 11:40:41 +0530

    Update Longest-unique-substring.py

--- Lists-and-dictionaries/Longest-unique-substring.py
+++ Lists-and-dictionaries/Longest-unique-substring.py
@@ -1,1 +1,24 @@
+def uniqueSubstring(string):
+  '''Returns largest unique substring'''
+  startL = 0
+  endL = 0
+  start = 0
+  1 = len(string)
+  while start<l:
+    d = {string[start]: start}
+    end = start+1
+    while end<l and string[end] not in d:
+      d[string[end]] = end
+      end += 1
+    if end-start > endL-startL:
+      startL = start
+      endL = end
+    if end == 1:
+      start = 1
+    else:
+      start = d[string[end]] + 1
+  return string[startL:endL
 
+# Main
+S = input()
+print(uniqueSubstring(s))
